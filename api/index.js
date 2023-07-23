61006de9b73106801cc4e84a384f02e21bd5aca0
Salomão Lima <138541437+snlimadev@users.noreply.github.com>
2023-07-22 23:53:22 -0300

    Alterando index.js
    Comentando trecho desnecessário.

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
+/*const bodyParser = require('body-parser');
+app.use(bodyParser.json());*/
 
 const cors = require('cors');
 app.use(cors());
